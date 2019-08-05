e0f8662c0117ad3238bec781d9b62a2565f0fccd
Peter Jang <peter.x.jang@gmail.com>
2019-08-05 18:46:12 -0500

    Install mapbox on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div id="map"></div>
   </div>
 </template>
+
+<style>
+#map {
+  height: 400px;
+}
+</style>
+
+<script>
+/* global mapboxgl */
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  mounted: function() {
+    mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN;
+    var map = new mapboxgl.Map({
+      container: "map", // container id
+      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+    });
+  },
+  methods: {}
+};
+</script>
