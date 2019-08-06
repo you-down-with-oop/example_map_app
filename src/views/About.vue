66b496e22815ca658d8d1226150ac5c4b6030457
Peter Jang <peter.x.jang@gmail.com>
2019-08-05 20:40:22 -0500

    Use Vue.js data for marker info

--- src/views/About.vue
+++ src/views/About.vue
@@ -16,7 +16,24 @@
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      places: [
+        {
+          lat: 37.974728,
+          long: -122.03711,
+          description: "Alpine Bakery in Concord, delicious pastries!"
+        },
+        {
+          lat: 36.973804,
+          long: -122.02575,
+          description: "Marinis is my favorite ice cream in Santa Cruz"
+        },
+        {
+          lat: 37.791852,
+          long: -122.42127,
+          description: "Bob's Donuts in SF makes gigantic donuts, good for sharing (or eating alone!)"
+        }
+      ]
     };
   },
   mounted: function() {
@@ -28,12 +45,29 @@ export default {
       zoom: 9 // starting zoom
     });
 
-    var popup = new mapboxgl.Popup({ offset: 25 }).setText("Construction on the Washington Monument began in 1848.");
+    // var popup = new mapboxgl.Popup({ offset: 25 }).setText("Construction on the Washington Monument began in 1848.");
 
-    var marker = new mapboxgl.Marker()
-      .setLngLat([30.5, 50.5])
-      .setPopup(popup)
-      .addTo(map);
+    // var marker = new mapboxgl.Marker()
+    //   .setLngLat([30.5, 50.5])
+    //   .setPopup(popup)
+    //   .addTo(map);
+
+    this.places.forEach(function(place) {
+      var popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+      var marker = new mapboxgl.Marker()
+        .setLngLat([place.long, place.lat])
+        .setPopup(popup)
+        .addTo(map);
+    });
+
+    // for (var i = 0; i < this.places.length; i++) {
+    //   var place = this.places[i];
+    //   var popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+    //   var marker = new mapboxgl.Marker()
+    //     .setLngLat([place.long, place.lat])
+    //     .setPopup(popup)
+    //     .addTo(map);
+    // }
   },
   methods: {}
 };
